0bd7ab0b31c6e340903b41298f61600c2007591e
Renan Carletti <carletti95@gmail.com>
2024-10-28 15:15:04 -0300

    Estruturação da pagina

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
+
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route exact path="/projects" Component={Projects} />
+          <Route exact path="/company" Component={Company} />
+
+          <Route exact path="/contact" Component={Contact} />
+
+          <Route exact path="/newproject" Component={NewProject} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
